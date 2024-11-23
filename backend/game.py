599229de7fd4dd54b46623a316dea8bec21311ff
denniskaydalov <denniskaydalov@gmail.com>
2024-11-23 16:30:38 -0500

    Add current market info

--- backend/game.py
+++ backend/game.py
@@ -1,5 +1,5 @@
 from flask import Flask, Response 
-from client import trading_client
+from client import trading_client, MarketInfo
 from model import event, GameOrderBook
 
 app = Flask(__name__)
@@ -41,11 +41,24 @@ def get_client_by_name(name):
 
 @app.route("/start")
 def start():
-    global game_started, event_timeline, quotes
+    global game_started, event_timeline, quotes, securities_stories
+
+    clients = [
+            trading_client(name = "penguins", starting_balance = 100),
+            trading_client(name = "foxes", starting_balance = 100),
+            trading_client(name = "mammoths", starting_balance = 100),
+            ]
 
     event_timeline = create_event_timeline()
 
-    securities = ["Banana", "Coconut"]
+    securities = ["ice", "fish"]
+
+    securities_stories = {
+            "ice" : "The sun's rays grow stronger each day, and whispers spread through the market of a great heat wave approaching. The cloud shepherds have been seen guiding their flocks far to the north, leaving the southern markets exposed to the sun's fierce embrace.",
+
+            "fish" : "A mysterious current has shifted in the deep waters, bringing schools of silver-scaled visitors to unusual waters. The wise seals speak of a great migration that happens once every blue moon, but the local fishing fleets seem oddly quiet about their recent catches."
+
+            }
 
     quotes = GameOrderBook(securities = securities)
 
@@ -55,18 +68,27 @@ def start():
 
 @app.route("/tick")
 def tick():
-    global game_started, current_tick, clients, event_timeline, news_feed, securities, quotes
+    global game_started, current_tick, clients, event_timeline, news_feed, securities, quotes, securities_stories
+
+    current_market_info = dict() #= MarketInfo(story = "", 
+                        #             orderbook)
+
+    for security in securities:
+        #             orderbook="Bid: 30(300), 29(500)\nAsk: 31(200), 32(400)"
+
+        current_market_info[security] = MarketInfo(story = securities_stories[security],
+                                                   orderbook = ""
+                                                   )
 
     if not game_started:
         return ("Game has not started", 403)
-
 
     for event in event_timeline:
         if event.tick_start == current_tick:
             news_feed.append(event.event_description)
 
     for client in clients:
-        quotes.addOrder(clients[client].get_quote())
+        quotes.addOrder(clients[client].get_quote(market_info = current_market_info))
 
     resolved = quotes.fullfillOrders(tick = current_tick)
 

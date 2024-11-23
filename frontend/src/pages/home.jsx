d5d92af0ca188045d2ace6ead241d08005ea504e
micic-mihajlo <m2micic@uwaterloo.ca>
2024-11-23 18:08:55 -0500

    frontend banger

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,5 +1,5 @@
 import Header from '../components/Header'
-import HeroSection from '../components/HeroSection'
+import { HeroSection } from '../components/HeroSection'
 import TeamSection from '../components/TeamSection'
 import AssetSection from '../components/AssetSection'
 import TradingPit from '../components/TradingPit'

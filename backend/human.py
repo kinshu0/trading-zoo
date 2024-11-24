e4416f23b7ea3b7a3293512409943d4549e38151
Kinshu Gupta <kinshugupta2002@gmail.com>
2024-11-24 01:45:47 -0500

    human user trading implemented

---
+++ backend/human.py
@@ -0,0 +1,21 @@
+from typing import Dict, List
+from model import Order
+
+from client import security_details
+
+class HumanClient:
+    def __init__(self, team_name: str, starting_balance : int, state):
+        self.team_name = 'Hooman'
+
+        self.portfolio : List[security_details] = [] 
+        self.balance_available = starting_balance
+        self.state = state
+    
+    def get_portfolio_valuation(self):
+        return sum([sec.price * sec.quantity for sec in self.portfolio]) + self.balance_available
+        
+    def get_quote(self, securities : list, portolio_securities : list, quotes, current_tick : int, current_balance : int) -> List[Order]:
+        return self.state['orders']
+
+    def get_name(self):
+        return self.team_name
